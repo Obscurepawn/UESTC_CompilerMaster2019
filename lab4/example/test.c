8db1576d28db072e191aa84021acf2a0452cbcf2
Obscurepawn <2018091620007@std.uestc.edu.cn.com>
2019-12-21 13:02:56 +0800

    update my README.md

---
+++ lab4/example/test.c
@@ -0,0 +1,101 @@
+int aaa = 33, bbb, cccc = 44;
+int bbb = 3;
+str aa;
+str bbbb = "12345";
+
+int main()
+{
+	int a = 1;
+	int b = 1;
+	a = b*3;
+	b = a+a;
+	b = a/b;
+	a = b+b+b+b;
+	a /= 1;
+	b *= a;
+	b += b;
+	b -= a+b;
+	a = 888 +12;
+	aa = "ccc";
+	bbbb[2] = aa[0];
+	return 0;
+}
+
+void f(str k)
+{
+	str b ;
+    b = "aaa";
+	str c = "ccc" ;
+	str d = b + c;
+	d = k;
+}
+
+
+
+str mm(str k)
+{
+	int a = 1;
+	int b = 1;
+	a = b*2;
+	int c = a + b;
+	int mc = c*a;
+	int mk = c/a;
+	int mmmmp = c-a;
+	int kkkkl = 4/2;
+	int ere = 4*2;
+	int ccc = 4-2;
+	int mmmmmmmp = 8%2;
+	int mmp = c%a;
+	return k;
+}
+
+
+// int func1(int fir, int sec)
+// {
+// 	int b = fir*sec + 3000;
+// 	int a = 10;
+// 	while(a > 0)
+// 	{
+// 		str k = "\t" + a + "\n" ;
+// 		a = a -1;
+// 	}
+// 	//print "func1 done" + "\n" ;
+// 	return b;
+// }
+
+
+// void multable()
+// {
+// 	int i = 1;
+// 	while(i < 10)
+// 	{
+// 		int j = 1;
+// 		while(j < 10)
+// 		{
+// 			print "\t" + i*j;
+// 			j = j + 1;
+// 		}
+// 		print "\n";
+// 		i = i +1;
+// 	}
+// }
+
+// int main()
+// {
+// 	multable();
+// 	return 0;
+// }
+
+// int fn(int n)
+// {
+// 	if(n == 1)
+// 		return 1;
+// 	else
+// 	{
+// 		int r = n* fn(n-1);
+// 		print r;
+// 		//return r;
+// 	}
+// 	int b = 0;
+// 	return b;
+// }
